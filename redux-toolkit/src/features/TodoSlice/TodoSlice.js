3e96962705df2899e1e7c6f6bb1a93cc725a0e6b
PiyushGhavghave <piyushghavghave@gmail.com>
2024-11-23 01:39:20 +0530

    added local storage

--- redux-toolkit/src/features/TodoSlice/TodoSlice.js
+++ redux-toolkit/src/features/TodoSlice/TodoSlice.js
@@ -1,7 +1,15 @@
 import { createSlice ,nanoid} from "@reduxjs/toolkit";
 
+const getLocalData = () => {
+    const data = localStorage.getItem('todos')
+    return data ? JSON.parse(data) : [];
+}
+const setLocalData = (data) => {
+    localStorage.setItem('todos' , JSON.stringify(data))
+}
+
 const initialState = {
-    todos : []
+    todos : getLocalData()
 }
 
 export const todoSlice = createSlice({
@@ -15,21 +23,25 @@ export const todoSlice = createSlice({
                 completed : false
             }
             state.todos.unshift(newTodo);
+            setLocalData(state.todos)
         },
         deleteTodo : (state, action) => {
             state.todos = state.todos.filter((todo) => (todo.id !== action.payload))
+            setLocalData(state.todos)
         },
         updateTodo : (state, action) => {
             const {id , newText} = action.payload;
             state.todos = state.todos.map((todo) => (
                 todo.id === id ? {...todo, todoText : newText} : todo 
             ))
+            setLocalData(state.todos)
 
         },
         toggleCheck : (state, action) => {
             state.todos = state.todos.map((todo) => (
                 todo.id === action.payload ? {...todo, completed : !todo.completed} : todo
             ))
+            setLocalData(state.todos)
         }
     }
 })
